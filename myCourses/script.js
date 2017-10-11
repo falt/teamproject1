d7b1465771b64e3d9fb953fee4da9d6b30c5fede
Andreas Fält <falt@Andreass-MacBook-Pro.local>
2017-10-11 14:59:41 +0200

    Cleanup css

--- myCourses/script.js
+++ myCourses/script.js
@@ -4,12 +4,17 @@ let arrows = document.getElementsByClassName("arrow");
 
 for(let i = 0; i < cardButtons.length; i++) {
     cardButtons[i].addEventListener("click", function(){
-        if(cardContainers[i].style.height != "300px") {
-            cardContainers[i].style.height = "300px";
-            arrows[i].className = "arrow fa fa-sort-asc";             
+        if(cardContainers[i].style.height != "400px") {
+            cardContainers[i].style.height = "400px";
+            arrows[i].className = "arrow fa fa-sort-asc"; 
+            arrows[i].style.top = "5px"
+         
         } else {
             cardContainers[i].style.height = "40px"; 
-            arrows[i].className = "arrow fa fa-sort-desc";             
+            arrows[i].className = "arrow fa fa-sort-desc";
+         	arrows[i].style.top = "-5px"   
+
         }
     });
 }
+
