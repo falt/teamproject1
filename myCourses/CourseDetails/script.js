85f7cd01b61b96bd6b565969f64d1cf644839b67
Daniel Öhrn <Daniel.ohrnhasslof@student.kyh.se>
2017-10-19 10:44:11 +0200

    Add 'tabs' to course details page.

--- myCourses/CourseDetails/script.js
+++ myCourses/CourseDetails/script.js
@@ -15,3 +15,22 @@ function ExpandToggle(){
     window.scrollTo(0, 0);
   }
 }
+
+
+// COURSE-NAV-BAR
+const nav = document.querySelectorAll(".course-type-nav-bar li"); 
+const courseCategoryElem = document.querySelectorAll(".articleClass"); 
+
+for(let i = 0; i < nav.length; i++) {
+  nav[i].addEventListener("click", (e)=>{
+        
+    for(let j = 0; j < nav.length; j++) {
+      if(e.target == nav[j]) {
+        nav[j].style.background = "white"; 
+      } else {
+        nav[j].style.background = "initial"; 
+      }
+    }
+
+  }); 
+}
