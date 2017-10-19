3f4dce52890435bfd28a3c14703920023b097e85
Jeowi <32056595+Jeowi@users.noreply.github.com>
2017-10-19 10:29:57 +0200

    styling changes to the page. Expaning button on read-more

--- profile/studentprofile/studentprofile.js
+++ profile/studentprofile/studentprofile.js
@@ -1,11 +1,22 @@
-let readMore = document.getElementsByClassName("read-more);
-let newsContainer = document.getElementsByClassName("article")
-let 
+// function changeStyle(e) {
+//     e.preventDefault();
+//     var status = 1;
+//     var button = document.getElementById("text");
+    
+//     if(status==1) {
+//         button.style.color = 'green';
+//         console.log("Skickat!")
+//     }
+    
+//     else {
+//         button.style.color = 'red';
+//         console.log("Fyll i alla fält.")
+//     }
 
-function showHide(elementid){
-    if (document.getElementById(elementid).style.display == 'none'){
-        document.getElementById(elementid).style.display = '';
-    } else {
-        document.getElementById(elementid).style.display = 'none';
-    }
-}
+const buttons = document.querySelectorAll('.read-more1')
+buttons.forEach( function(button) {
+    button.addEventListener('click', function(e) {
+        e.target.previousSibling.previousSibling.classList.toggle('hidden')
+    })
+})
+
