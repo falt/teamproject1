527789c8cd92261d6500fe6a21c547e63f1985dc
Andreas Fält <falt@Andreass-MacBook-Pro.local>
2017-10-24 12:23:26 +0200

    change behavior of sedn button on weekly eval

--- profile/studentprofile/attendance.js
+++ profile/studentprofile/attendance.js
@@ -1,3 +1,5 @@
+//Attendance function, hiding/ showing elements and checking value for attendacne
+
 document.getElementById('attendanceButton').addEventListener("click",function(e){
     e.preventDefault()
     attendance()
@@ -14,11 +16,24 @@ function attendance() {
     if(attendanceInput == "abc123"){
         // let textfield = document.getElementById("attendanceInput");
         // let approved = document.getElementById("attendanceApproved");
-        textfield.classList.toggle("attendanceChange");
-        approved.classList.toggle("attendanceChange");
-        button.classList.toggle("attendanceChange")
+        textfield.classList.toggle("hide");
+        approved.classList.toggle("hide");
+        button.classList.toggle("hide")
         console.log("rätt")
     } else {
         console.log("fel")
     }
+}
+
+//send button function
+document.getElementById('sendButton').addEventListener("click",function(e){
+    e.preventDefault()
+    send()
+} );
+
+function send(){
+    let button = document.getElementById("sendButton");
+    let approved = document.getElementById("sendWeekly");
+    button.classList.toggle("hide");
+    approved.classList.toggle("hide");
 }
---
+++ profile/studentprofile/sendbutton.js
@@ -0,0 +1,4 @@
+document.getElementById('sendButton').addEventListener("click", function (e) {
+    e.preventDefault()
+    attendance()
+});
