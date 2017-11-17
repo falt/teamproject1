1803cc0513da28315704e6ec0cdee9db3c7f8cb9
falt <falt.andreas@gmail.com>
2017-11-17 13:24:52 +0100

    add to javascript

--- profile/adminprofile/adminTools/createClass.js
+++ profile/adminprofile/adminTools/createClass.js
@@ -13,32 +13,32 @@ let email = document.getElementById("email");
 let classBtn = document.getElementById("classBtn");
 let studentBtn = document.getElementById("studentBtn");
 
-
+//creating a object whit info about the class
 function createClass(className, city, adminName) {
     this.name = className;
     this.city = city;
     this.admin = adminName;
     this.students = [];
 }
-
+//creating a object whit info about each student
 function createStudent(studentName, tel, email) {
     this.name = studentName;
     this.tel = tel;
     this.email = email;
 }
 function createStudentCard(studentName, tel, email) {
-    const div = document.createElement("div")
+    const div = document.createElement("div");
     div.className = "newStudent";
 
     const h2 = document.createElement("h2");
     h2.className = "studentName";
-    h2.innerHTML = "andreas"
+    h2.innerHTML = studentName.value;
     const pEmail = document.createElement("p");
     pEmail.className = "email";
-    pEmail.innerHTML = "falt.andreas@gmail.com";
+    pEmail.innerHTML = email.value;
     const pTele = document.createElement("p");
     pTele.className = "tele";
-    pTele.innerHTML = "0707";
+    pTele.innerHTML = tel.value;
 
     div.appendChild(h2);
     div.appendChild(pEmail);
@@ -46,16 +46,22 @@ function createStudentCard(studentName, tel, email) {
     console.log(div);
 }
 
+//using createClass function to create an class anc changing h1 to class name
 let c;
 classBtn.addEventListener('click', function () {
     c = new createClass(className.value, city.value, adminName.value);
     document.getElementById('classNameH1').innerHTML = className.value;
-
+    console.log(c);
 });
 
+//using createStudent function to create an new student an puts it in the class object
+let addStudent = document.getElementById("studentList");
 studentBtn.addEventListener('click', function () {
     let s = new createStudent(studentName.value, tel.value, email.value);
     c.students.push(s);
+    addStudent.innerHTML += createStudentCard(studentName, tel, email);
+
     console.log(s);
     console.log(c);
+    console.log(createStudentCard(studentName, tel, email));
 });
